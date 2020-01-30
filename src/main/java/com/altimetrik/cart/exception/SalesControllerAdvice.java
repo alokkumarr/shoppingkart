7353b2d33dae5cb51aabf2bdbe298198a3f7155a
alok.kumarr <alok.kumarr@synchronoss.com>
2020-01-30 20:55:15 +0530

    Global exception handler

---
+++ src/main/java/com/altimetrik/cart/exception/SalesControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.altimetrik.cart.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestControllerAdvice
+@Component
+public class SalesControllerAdvice {
+
+  @ExceptionHandler(value = Exception.class)
+  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+  public String handleUnknownException() {
+    return "Unexpected Error";
+  }
+
+  @ExceptionHandler(value = HttpClientErrorException.class)
+  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+  public ResponseEntity<Object> handleNotFoundException() {
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+}
+
